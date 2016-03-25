531c1695c68bf0c0c2c9dc193420003780b1e4d0
lanou3g <lanou3g@lanou3gdeMac-mini-4.local>
2016-03-25 15:03:11 +0800

    add1

---
+++ 第三方01/第三方01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  第三方01
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 lxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 第三方01/第三方01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  第三方01
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 lxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
